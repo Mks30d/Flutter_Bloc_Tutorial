4d33e31241cb3bbf39ec0a18afd2bc023408a07d
Mks30d <manish30d@gmail.com>
2025-02-18 16:09:33 +0530

    displayed products on home screen

--- lib/features/home/ui/home.dart
+++ lib/features/home/ui/home.dart
@@ -101,7 +101,7 @@ class _HomeState extends State<Home> {
                   itemCount: successState.products.length,
                   itemBuilder: (context, index) {
                     return ProductTileWidget(
-                        productDataModel: successState.products[index]);
+                        productDataModel: successState.products[index], homeBloc: homeBloc,);
                   }),
             );
           case HomeErrorState:
